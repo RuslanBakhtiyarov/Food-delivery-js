9f8acc87a9c54b8256c549f563dde4d038746bed
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2021-11-08 11:56:17 +0200

    4

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,36 @@
+const restaurant = 'tanuki'
+const cardsMenu = document.querySelector('.cards-menu')
+
+
+
 const renderItems = (data) => {
-    console.log(data);
+    data.forEach(item => {
+        const card = document.createElement('div')
+console.log(item);
+        card.classList.add('card')
+
+        card.innerHTML = `
+        <img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">Пицца Везувий</h3>
+							</div>
+							<div class="card-info">
+								<div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+									«Халапенье», соус «Тобаско», томаты.
+								</div>
+							</div>
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">545 ₽</strong>
+							</div>
+						</div>
+                        `
+       
+    });
 }
 
 
